60fcd09fce661761fea92850446faffff1b45ab8
Maxel <gigaabait@yandex.ru>
2020-07-12 18:42:27 +0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/need-for-drive/precache-manifest.33cedfc161c50ea4a95643904c76280b.js"
+  "/need-for-drive/precache-manifest.37a1a390ab419fa0ac21677b59eb3d23.js"
 );
 
 self.addEventListener('message', (event) => {
