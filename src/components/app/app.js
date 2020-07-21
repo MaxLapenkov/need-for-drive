601b93d4794f37db7776ea32c737303bb88a77d5
Maxel <gigaabait@yandex.ru>
2020-07-21 14:10:50 +0400

    changed styles derictory folder

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,20 +4,23 @@ import MapPage from '../order-pages/map-page';
 import ModelPage from '../order-pages/model-page';
 import ExtraPage from '../order-pages/extra-page';
 import FinalPage from '../order-pages/final-page';
+import '../../styles/style.scss'
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom'
 
 const App = () => {
     return (
-        <Router>
-            <Switch>
-                <Route path="/need-for-drive" component={MainPage}/>
-                <Route path="/order/map" component={MapPage}/>
-                <Route path="/order/model" component={ModelPage}/>
-                <Route path="/order/extra" component={ExtraPage}/>
-                <Route path="/order/final" component={FinalPage}/>
-                <Redirect from="/" to="/need-for-drive"/>
-            </Switch>
-        </Router>
+        <div className="app">
+            <Router>
+                <Switch>
+                    <Route path="/need-for-drive" component={MainPage}/>
+                    <Route path="/order/map" component={MapPage}/>
+                    <Route path="/order/model" component={ModelPage}/>
+                    <Route path="/order/extra" component={ExtraPage}/>
+                    <Route path="/order/final" component={FinalPage}/>
+                    <Redirect from="/" to="/need-for-drive"/>
+                </Switch>
+            </Router>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import '../src/assets/styles/style.scss';
 import App from './components/app';
 
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root')); 
